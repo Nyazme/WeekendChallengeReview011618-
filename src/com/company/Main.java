a74ed2eba48b2a831b32eb88c96c7af1b8004a38
Mark <feinmanmarke@gmail.com>
2018-01-16 11:41:14 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Boolean ask = true;
+        Scanner sc = new Scanner(System.in);
+
+        //while(ask){
+
+
+        System.out.println("Input First Number:");
+        String firstNumber = sc.nextLine();
+
+        System.out.println("Input one of the following valid operators: +, -, *, / :");
+        String operator = sc.nextLine();
+
+        System.out.println("Input Second Number:");
+        String secondNumber = sc.nextLine();
+
+        int intFirst = Integer.parseInt(firstNumber);
+        int intSecond = Integer.parseInt(secondNumber);
+        int intResult = 0;
+        //Double intFirst = Double.parseDouble (firstNumber);
+        //Double intSecond = Double.parseDouble (secondNumber);
+        //Double intResult = 0.0
+
+        if (operator.equals("+") || operator.equals("add")) {
+            intResult = intFirst + intSecond;
+        } else if (operator.equals("-") || operator.equals("subtract")) {
+            intResult = intFirst - intSecond;
+        } else if (operator.equals("*") || operator.equals("multiply")) {
+            intResult = intFirst * intSecond;
+        } else if (operator.equals("/") || operator.equals("divide")) {
+            intResult = intFirst / intSecond;
+
+
+        }
+
+        System.out.println("Your number is: " + intResult);
+        System.out.println("Would you like to try again?");
+        //String userResponse = sc.nextLine();
+
+//            if(userResponse.equalsIgnoreCase("yes") || userResponse.equalsIgnoreCase("y")){
+//                ask = true;
+//
+//            } else {
+//                ask = false;
+//            }
+        //}System.out.println("Have a nice day!");
+
+    }
+}
+
+
